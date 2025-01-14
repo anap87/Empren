b12f28212e1ea1b3646efffdf9d6d182def29107
anap87 <apor2209@gmail.com>
2025-01-14 09:19:36 -0600

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -58,7 +58,13 @@ loginForm.addEventListener('submit', (e) => {
         if (user.password === password) {
             localStorage.setItem('currentUser', JSON.stringify(user));
             alert(`¡Bienvenido, ${user.username}!`);
-            window.location.href = "portal.html";
+
+            // Verificar si el usuario es el único que tiene acceso a admin.html
+            if (email === 'apor2209@gmail.com' && password === '12345678Prueba') {
+                window.location.href = "admin.html"; // Redirigir al admin
+            } else {
+                window.location.href = "portal.html"; // Redirigir al portal del cliente
+            }
         } else {
             alert('Contraseña incorrecta. Inténtalo de nuevo.');
         }
